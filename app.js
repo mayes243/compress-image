b0d70430401d6138738c1ad5f6721e77d9f414c9
mayeskhatri243 <mayeskhatri243@gmail.com>
2023-11-27 11:40:02 +0530

    vercel config implemented

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
+import express from "express";
+import compressImages from "compress-images";
+import formidable from "express-formidable";
+import fileSystem from "fs";
+
 const app = express();
 
-const compressImages = require("compress-images");
-const formidable = require("express-formidable");
 app.use(formidable());
 
-const fileSystem = require("fs");
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
