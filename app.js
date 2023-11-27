9de6491195028e2d634cccda67afe5d3b747cc18
mayeskhatri243 <mayeskhatri243@gmail.com>
2023-11-27 12:35:24 +0530

    updated the static path

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(formidable());
 const tempDir = os.tmpdir();
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
